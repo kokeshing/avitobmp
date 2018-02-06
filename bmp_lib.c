823e5145ed91d23fdea39a00694baab5e8dc71ab
kokeshing <kou7cdb1a@gmail.com>
2018-02-06 22:41:46 +0900

    コメントの追加変更

--- bmp_lib.c
+++ bmp_lib.c
@@ -2,15 +2,15 @@
 
 // ファイルヘッダの初期化
 void initialize_fheader(bmp_data* data){
-    data->fheader.file_type[0] = 66;
-    data->fheader.file_type[1] = 77;
+    data->fheader.file_type[0] = 66; // B
+    data->fheader.file_type[1] = 77; // M
     data->fheader.file_size[0] = 54;
     data->fheader.file_size[1] = 140;
     data->fheader.file_size[2] = 10;
     data->fheader.file_size[3] = 0;
     data->fheader.reserved1 = 0;
     data->fheader.reserved1 = 0;
-    data->fheader.imagedata_offset[0] = 54;
+    data->fheader.imagedata_offset[0] = 54; // ファイルヘッダ+インフォヘッダ=54
     data->fheader.imagedata_offset[1] = 0;
     data->fheader.imagedata_offset[2] = 0;
     data->fheader.imagedata_offset[3] = 0;
@@ -18,16 +18,16 @@ void initialize_fheader(bmp_data* data){
 
 // 情報ヘッダの初期化
 void initialize_iheader(bmp_data* data){
-    data->iheader.header_size = 40;
-    data->iheader.width = 640;
-    data->iheader.height = 360;
+    data->iheader.header_size = 40; // インフォヘッダのバイト数
+    data->iheader.width = 640; // 横
+    data->iheader.height = 360; // 縦
     data->iheader.plane_num = 1;
-    data->iheader.bit_num = 24;
+    data->iheader.bit_num = 24; // bit数
     data->iheader.compression_type = 0;
     data->iheader.imagedata_size = 0;
     data->iheader.horizon_dpi = 0;
     data->iheader.vertical_dpi = 0;
-    data->iheader.color_index_num = 16777216;
+    data->iheader.color_index_num = 16777216; // 色数
     data->iheader.important_index_num = 0;
 }
 
